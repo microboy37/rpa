e6c16e2efc36b7a02a14164760f9f1426418fdf9
OtonMar <otonmar201@gmail.com>
2023-01-18 17:02:11 +0100

    prvi commit

---
+++ prva_vaja/prva_vaja/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prva_vaja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("xxx");
+            Console.ReadLine();
+        }
+    }
+}
