f84c136e7a5fbeecacc1ada8ee879e816a19b4d0
“airaction” <otonmar201@gmail.com>
2023-01-18 18:59:00 +0100

    ddd

--- vaja_elektrika/VajaElektrika/VajaElektrika/Program.cs
+++ vaja_elektrika/VajaElektrika/VajaElektrika/Program.cs
@@ -11,7 +11,18 @@ namespace VajaElektrika
         static void Main(string[] args)
         {
 
+            ElektrikaEntities1 db = new ElektrikaEntities1();
 
+            var x1 = db.Meritve.Select(a => new { a.ZapisČas, a.kW1 }).Take(10);
+
+            foreach (var x in x1)
+            {
+
+                Console.WriteLine(x.ZapisČas + " " + x.kW1);
+
+                Console.ReadLine();
+
+            }
         }
     }
 }
